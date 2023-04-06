eb026312442a452bb342671433aaaef869d5acdd
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-06 11:00:14 +0200

    nouveau dossier

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import '@/styles/global.scss'
+import "@/styles/custom.scss";
+import '@/styles/global.scss';
+import { useEffect } from "react";
 
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />
 }
